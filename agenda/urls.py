a553d57f9c9348e7fedb54c7b7e6f4a5e9cfbe56
Nicolly Ant�nia de Oliveira Souza <47819795808@>
2025-04-04 16:59:36 -0300

    teste

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from agenda.views import Agendamento,Servico
+from agenda.views import servicos,agendar
 urlpatterns = [    
-    path('api/servicos/', Servico, name='servicos'),
-    path('api/servicos/', Servico, name='servicos'),
-    path('api/servicos/<int:pk>', Servico, name='servicos'),
-    path('api/agendamentos/', Agendamento, name='servicos'),
-    path('api/agendamentos/', Agendamento, name='agendar'),
-    path('api/agendamentos/<int:pk>', Agendamento, name='agendar'),
+    path('api/servicos/', servicos, name='servicos'),
+    path('api/servicos/', servicos, name='servicos'),
+    path('api/servicos/<int:pk>', servicos, name='servicos'),
+    path('api/agendamentos/', agendar, name='servicos'),
+    path('api/agendamentos/', agendar, name='agendar'),
+    path('api/agendamentos/<int:pk>', agendar, name='agendar'),
 ]
